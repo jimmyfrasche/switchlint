4fecaa0dc669e51974de5b8479e2a9bcbb4da099
jimmyfrasche <soapboxcicero@gmail.com>
2017-05-04 11:42:51 -0700

    0

---
+++ README.md
@@ -0,0 +1,1 @@
+switchlint finds switches without default cases in specified go files. Based on https://github.com/golang/go/issues/19412#issuecomment-299111884
---
+++ switchlint.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	flag.Parse()
+
+	exitCode := 0
+	for _, f := range flag.Args() {
+		if err := inspect(f); err != nil {
+			log.Print(err)
+			exitCode = 1
+		}
+	}
+
+	os.Exit(exitCode)
+}
+
+func inspect(file string) error {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, file, nil, 0)
+	if err != nil {
+		return err
+	}
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		//no default case for irony and so it can be tested on itself
+		switch n := n.(type) {
+		case *ast.TypeSwitchStmt:
+			check(fset.Position(n.Switch), n.Body)
+		case *ast.SwitchStmt:
+			if n.Tag != nil {
+				check(fset.Position(n.Switch), n.Body)
+			}
+		}
+		return true
+	})
+
+	return nil
+}
+
+func check(p token.Position, body *ast.BlockStmt) {
+	foundDefault := false
+	for _, block := range body.List {
+		block := block.(*ast.CaseClause)
+		if block.List == nil {
+			foundDefault = true
+			break
+		}
+	}
+	if !foundDefault {
+		fmt.Printf("%s: no default case\n", p)
+	}
+}
